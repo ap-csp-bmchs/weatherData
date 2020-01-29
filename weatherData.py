629b77bfeb07565bc158a88830b770bff8b34005
Jacob Holman <jeholman21@bmchs.org>
2020-01-29 07:15:05 -0800

    Final edit.

---
+++ weatherData.py
@@ -0,0 +1,22 @@
+import csv
+
+max_list = []
+min_list = []
+
+with open("Norman2016.csv", "r") as csv_file:
+    reader = csv.DictReader(csv_file, delimiter = ",")
+    for row in reader:
+        minimum = row["TMIN"]
+        maximum = row["TMAX"]
+        if float(maximum) > -10.0:
+            max_list.append(float(row["TMAX"]))
+        if float(minimum) > -10.0:
+            min_list.append(float(row["TMIN"]))
+
+print("The maximum temp in 2016 was", max(max_list))
+
+print("The minimum temp in 2016 was", min(min_list))
+
+print("The avg high temp in 2016 was", sum(max_list)/len(max_list))
+
+print("The avg low temp in 2016 was", sum(min_list)/len(min_list))
